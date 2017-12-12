10259be6f749e074d2722b194248197acea33999
Thipus <nicolas.lecointre.2@gmail.com>
2017-12-12 18:15:05 +0100

    debut agreabilité zone

--- model.py
+++ model.py
@@ -57,6 +57,8 @@ class Zone:
     @classmethod
     def find_zone_that_contains(cls, position):
         # Compute the index in the ZONES array that contains the given position
+        if not cls.ZONES:
+        	cls._initialize_zones()
         longitude_index = int((position.longitude_degrees - cls.MIN_LONGITUDE_DEGREES)/ cls.WIDTH_DEGREES)
         latitude_index = int((position.latitude_degrees - cls.MIN_LATITUDE_DEGREES)/ cls.HEIGHT_DEGREES)
         longitude_bins = int((cls.MAX_LONGITUDE_DEGREES - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES) # 180-(-180) / 1
@@ -69,7 +71,7 @@ class Zone:
         return zone
 
     @classmethod
-    def initialize_zones(cls):
+    def _initialize_zones(cls):
         # Note that this method is "private": we prefix the method name with "_".
         cls.ZONES = []
         for latitude in range(cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
@@ -85,7 +87,6 @@ def main():
 		longitude = agent_attributes.pop('longitude')
 		position = Position(longitude, latitude)
 		agent = Agent(position, **agent_attributes)
-		Zone.initialize_zones()
 		zone = Zone.find_zone_that_contains(position)
 		zone.add_inhabitant(agent)
 		print(zone.population)
